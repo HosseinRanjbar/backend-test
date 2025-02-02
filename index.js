d5daa8eeecd17060c0e8f0f0050931aba7227e25
Sahab Ranjbar <amirisahab@gmail.com>
2025-02-03 00:44:52 +0330

    third commitaasss

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
 const express = require('express');
-// const cors = require('cors');
+const cors = require('cors');
 const app = express();
 
-// // Install cors first: npm install cors
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
-// // Option 1: Allow specific origin
-// app.use(cors({
-//     origin: 'http://localhost:5173',
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     allowedHeaders: ['Content-Type', 'Authorization']
-// }));
+app.use(cors());
 
-// Option 2: Allow all origins (development only)
-// app.use(cors());
-// const API_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ODg3MDZjOTY3MDliNjVmNjhjMzU0OGZiMzJjZTI1MyIsIm5iZiI6MTcyNjUxMjA2NS4wODgsInN1YiI6IjY2ZTg3YmMxMDUwZjE0ZTRmY2QwMjQ4ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RERGdrEkjlzEDi9WMDDOjJpnUh5bOCM8VHLiSvvjhKE"
+const API_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ODg3MDZjOTY3MDliNjVmNjhjMzU0OGZiMzJjZTI1MyIsIm5iZiI6MTcyNjUxMjA2NS4wODgsInN1YiI6IjY2ZTg3YmMxMDUwZjE0ZTRmY2QwMjQ4ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RERGdrEkjlzEDi9WMDDOjJpnUh5bOCM8VHLiSvvjhKE"
+
 app.get('/', (req, res) => {
-    // fetch("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc", {
-    //     method: 'GET',
-    //     headers: {
-    //         accept: 'application/json',
-    //         Authorization: `Bearer ${API_KEY}`
-    //     }
-    // }).then(res => res.json()).then(data => {
-    //     res.send(data);
-    // })
+    fetch("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc", {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${API_KEY}`
+        }
+    }).then(res => res.json()).then(data => {
+        res.send(data);
+    })
 
-    res.send("hello")
 
 });
 
